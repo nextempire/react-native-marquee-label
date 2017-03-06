dd77dce86f98b0fdfeb8b273161f0d18e6698eda
IngoValente <i.valente@gmail.com>
2017-03-06 13:42:59 +0100

    Correct proptypes
    Fixes warning for incorrect proptypes

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict';
 
-var React = require('react');
-var {
-    PropTypes,
-} = React;
-var ReactNative = require('react-native');
-var {
-    View,
-    requireNativeComponent
-} = ReactNative;
+import React, { Component, PropTypes } from 'react';
+import { View, requireNativeComponent } from 'react-native';
 
-var MarqueeLabel = React.createClass({
-    propTypes: {
+export class MarqueeLabel extends Component {
+    propTypes = {
         ...View.propTypes,
         text : PropTypes.string.isRequired,
         scrollDuration : PropTypes.number, //秒
-        marqueeType : PropTypes.number, //ios
+        marqueeType : PropTypes.string, //ios
         fadeLength : PropTypes.number, //ios
         leadingBuffer : PropTypes.number, //ios
         trailingBuffer : PropTypes.number, //ios
@@ -23,9 +16,10 @@ var MarqueeLabel = React.createClass({
         isRepeat : PropTypes.bool, //android
         startPoint : PropTypes.number, //android
         direction : PropTypes.number, //android
-    },
-    render: function() {
-        const {children, ...props} = this.props;
+    }
+
+    render() {
+        const { children, ...props } = this.props;
         const nativeProps = Object.assign({}, props, {text: children});
         return (
             <RCTMarqueeLabel  {...nativeProps}/>
@@ -33,5 +27,4 @@ var MarqueeLabel = React.createClass({
     }
 });
 
-var RCTMarqueeLabel = requireNativeComponent('RCTMarqueeLabel', MarqueeLabel);
-module.exports = MarqueeLabel;
+const RCTMarqueeLabel = requireNativeComponent('RCTMarqueeLabel', MarqueeLabel);
